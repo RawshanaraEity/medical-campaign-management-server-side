2ea2f1363dbf9e0f540fd18ed9cceb564b213d75
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-11-27 22:40:21 +0600

    added get data api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkpsd7x.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +29,18 @@ async function run() {
 
     const campCollection = client.db('medicalCampDB').collection('camps')
 
-
+    // camp related api
     app.get('/camps', async(req, res) =>{
         const result = await campCollection.find().toArray();
         res.send(result)
     })
 
+    app.get('/camps/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await campCollection.findOne(query);
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
