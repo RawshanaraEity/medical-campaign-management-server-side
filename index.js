d3f7364fb35ef780ae48ecc9f322ea9fd08c9850
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-11-27 22:47:34 +0600

    added get specifiq data api with id

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/camps/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await campCollection.findOne(query);
+        res.send(result)
+    })
+
   
 
 
