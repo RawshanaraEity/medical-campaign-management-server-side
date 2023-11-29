d8b660b6360e7420ebc619a80b02a2eb3bfc54c3
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-11-29 17:57:13 +0600

    added get users api in server

--- index.js
+++ index.js
@@ -97,6 +97,26 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/users', async(req, res) =>{
+        const result = await userCollection.find().toArray();
+        res.send(result)
+    })
+
+
+    app.get('/users/organizer/:email', async(req, res) =>{
+        const email = req.params.email;
+        const query = {email: email}
+        const user = await userCollection.findOne(query);
+        let organizer = false;
+        if (user?.role === 'organizer') {
+            organizer = true;
+        }
+        res.send({ success: true, organizer })
+
+    })
+
+   
+
     // register related api
     app.post('/register', async(req, res) =>{
         const registeredUser = req.body;
